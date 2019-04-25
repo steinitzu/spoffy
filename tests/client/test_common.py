62a423c75687ba8f8a7f40476ee835332e3805b0
Steinthor Palsson <steini90@gmail.com>
2019-04-25 17:21:16 -0400

    Add some tests

---
+++ tests/client/test_common.py
@@ -0,0 +1,54 @@
+from collections import namedtuple
+
+from spoffy.client.common import ClientCommon
+from spoffy.models import Track
+
+
+class TestPrepareRequest:
+    def test__next_url__replaces_prefix(self):
+        c = ClientCommon()
+        c.base_url = "https://example.com"
+
+        result = c.prepare_request(
+            "GET",
+            "https://api.spotify.com/v1/me/following?type=artist&limit=1",
+        )
+
+        assert (
+            result.url
+            == "https://example.com/me/following?type=artist&limit=1"
+        )
+
+    def test__url_no_prefix__adds_base_url(self):
+        c = ClientCommon()
+        url = "/me/following?type=artist"
+
+        result = c.prepare_request("GET", url)
+
+        assert result.url == c.base_url + url
+
+    def test__no_token__adds_client_access_token(self):
+        c = ClientCommon(access_token="sometoken")
+        url = "/me"
+
+        result = c.prepare_request("GET", url)
+
+        assert result.headers["Authorization"] == "Bearer sometoken"
+
+
+class TestLoad:
+    def test__none_target__returns_none(self):
+        c = ClientCommon()
+
+        FResponse = namedtuple("Response", ["json"])
+        resp = FResponse(json=dict(a=1))
+
+        assert c.load(resp, None) is None  # type:ignore
+
+    def test__none_data__returns_none(self):
+        c = ClientCommon()
+
+        FResponse = namedtuple("Response", ["json"])
+        resp = FResponse(json=None)
+
+        assert c.load(resp, Track) is None  # type:ignore
---
+++ tests/test_util.py
@@ -0,0 +1,24 @@
+import pytest
+
+from spoffy.util import get_page_url
+from spoffy.models.base import SpotifyObject
+
+
+class TestGetPageUrl:
+    def test__finds_next_at_root_level(self):
+        obj = SpotifyObject(next="https://example.com")  # type:ignore
+
+        assert get_page_url(obj, "next") == obj.next  # type:ignore
+
+    def test__finds_next_under_artists(self):
+        obj = SpotifyObject(  # type:ignore
+            artists=SpotifyObject(next="https://example.com")  # type:ignore
+        )
+
+        assert get_page_url(obj, "next") == obj.artists.next  # type:ignore
+
+    def test__none_found__raises_valueerror(self):
+        obj = SpotifyObject(foo=SpotifyObject(id="asdasds"))  # type:ignore
+
+        with pytest.raises(AttributeError):
+            get_page_url(obj, "next")
